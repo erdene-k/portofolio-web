58d340d0eeb139e2168d71960f2f684a3b2fb3c5
erd <erdenechuluun.kh@tavanbogd.com>
2024-03-15 14:35:23 +0800

    init

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Link from "next/link";
+
+const Navbar = () => {
+  const linkStyle = "rounded-lg border-color-border px-3.5 py-2 hover:border-2";
+  return (
+    <nav className="fixed left-1/2 top-8 -translate-x-1/2 -translate-y-1/2 transform">
+    <ul className="flex h-16 items-center justify-center gap-6">
+      <li className={linkStyle}>
+        <Link href="#">
+          <p>Work</p>
+        </Link>
+      </li>
+      <li className={linkStyle}>
+        <Link href="#">
+          <p>About</p>
+        </Link>
+      </li>
+      <li className={linkStyle}>
+        <Link href="#">
+          <p>Project</p>
+        </Link>
+      </li>
+      <li className={linkStyle}>
+        <Link href="#">
+          <p>Contact</p>
+        </Link>
+      </li>
+    </ul>
+  </nav>
+  );
+};
+
+export default Navbar;
